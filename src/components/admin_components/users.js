a1a580c1f899c653f581d0de7e3f15cabbe5182d
kdrahul <rahul.kulks@gmail.com>
2021-01-06 17:14:56 +0530

    Added insert user form

--- src/components/admin_components/users.js
+++ src/components/admin_components/users.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 const Users = () => {
   const [items, setItems] = useState([]);
+  const { register, handleSubmit, reset } = useForm();
+
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios.get("http://localhost:8989/users/");
@@ -9,11 +12,148 @@ const Users = () => {
     };
     fetchData();
   }, []);
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    const result = await axios.post("http://localhost:8989/users/", data);
+    const newItems = [...items, result.data];
+    setItems(newItems);
+
+    reset({});
+  };
+
   return (
     <div>
       <div>
-        <button className="btn btn-primary mb-2">+ Add User</button>
+        <button
+          className="btn btn-primary mb-2"
+          data-toggle="modal"
+          data-target="#userModal"
+        >
+          + Add User
+        </button>
       </div>
+
+      <div
+        className="modal fade"
+        id="userModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="userModalLabel"
+        aria-hidden="true"
+      >
+        <form>
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="userModalLabel">
+                  New User
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="form-group">
+                  <label>Username</label>
+                  <input
+                    name="username"
+                    type="text"
+                    className="form-control"
+                    ref={register}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input
+                    name="password"
+                    type="text"
+                    className="form-control"
+                    ref={register}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>First Name</label>
+                  <input
+                    name="first_name"
+                    type="text"
+                    className="form-control"
+                    ref={register}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Last Name</label>
+                  <input
+                    name="last_name"
+                    type="text"
+                    className="form-control"
+                    ref={register}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Phone Number</label>
+                  <input
+                    name="phone"
+                    type="text"
+                    className="form-control"
+                    ref={register}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>E-mail</label>
+                  <input
+                    name="email"
+                    type="text"
+                    className="form-control"
+                    ref={register}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Branch</label>
+                  <input
+                    name="branch"
+                    type="text"
+                    className="form-control"
+                    ref={register}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Role</label>
+                  <input
+                    name="role"
+                    type="text"
+                    className="form-control"
+                    ref={register}
+                  />
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  onClick={handleSubmit(onSubmit)}
+                  type="button"
+                  className="btn btn-primary"
+                  data-dismiss="modal"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+
       <table className="table">
         <thead>
           <tr>
@@ -29,7 +169,7 @@ const Users = () => {
         </thead>
         <tbody>
           {items.map((userDetail) => (
-            <tr>
+            <tr key={userDetail.id}>
               <th scope="row">{userDetail.id}</th>
               <td>{userDetail.username}</td>
               <td>{userDetail.first_name}</td>
